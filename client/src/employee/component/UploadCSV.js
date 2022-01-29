64c0acc0bd71ba781969d30cd525a02d5e6c8de3
Asif <1421015@iub.edu.bd>
2022-01-29 13:08:15 +0600

    [in progress] UI upload CSV

--- client/src/employee/component/UploadCSV.js
+++ client/src/employee/component/UploadCSV.js
@@ -2,6 +2,8 @@ import React, {useState,useContext} from 'react'
 import {Navigate as Redirect} from 'react-router-dom'
 import {Button,Typography,TextField,Card,CardActions,CardContent,Icon,} from '@mui/material';
 import {makeStyles}  from '@mui/styles';
+import { parse } from "papaparse";
+
 const useStyles = makeStyles(theme => ({
   card: {
     maxWidth: 600,
@@ -25,23 +27,66 @@ const useStyles = makeStyles(theme => ({
   submit: {
     margin: 'auto',
     marginBottom: theme.spacing(2)
-  }
+  },
+  dropZone:{
+    width:200,padding:200,
+    
+    backgroundColor:'grey'
+  },
+  dropZoneHighlight:{
+    width:200,padding:200,
+    opacity: 0.3,
+    
+    backgroundColor:'green'
+  },
+
 }))
 
 export default function Signin(props) {
 
+  const [highlighted, setHighlighted] = React.useState(false);
+  const [contacts, setContacts] = React.useState([
+    { email: "fake@gmail.com", name: "Fake" },
+  ]);
+  console.log("my contacts ",contacts);
   const classes = useStyles()
  
   return (
       <Card className={classes.card}>
         <CardContent>
           <Typography variant="h6" className={classes.title}>
-            Sign In
+           Add Employee Details
           </Typography>
  
         </CardContent>
         <CardActions>
-          <Button color="primary" variant="contained"  className={classes.submit}>Upload CSV file</Button>
+               <div
+          className= {highlighted ?classes.dropZoneHighlight: classes.dropZone}
+      
+        onDragEnter={() => {
+          setHighlighted(true);
+        }}
+        onDragLeave={() => {
+          setHighlighted(false);
+        }}
+        onDragOver={(e) => {
+          e.preventDefault();
+        }}
+        onDrop={(e) => {
+          e.preventDefault();
+          setHighlighted(false);
+
+          Array.from(e.dataTransfer.files)
+            .filter((file) => file.type === "text/csv")
+            .forEach(async (file) => {
+              const text = await file.text();
+              const result = parse(text, { header: true });
+              setContacts((existing) => [...existing, ...result.data]);
+            });
+        }}
+      >
+       <Typography style={{color:"white", fontSize:30}}> DROP HERE</Typography>
+      </div>
         </CardActions>
         <CardContent>
           <Typography variant="h6">
@@ -55,7 +100,16 @@ export default function Signin(props) {
           <Button color="primary" variant="contained"  className={classes.submit}>Submit</Button>
         </CardActions>
         </CardContent>
+        <CardContent>
+
+      <ul>
+        {contacts.map((contact) => (
+          <li key={contact.email}>
+            <strong>{contact.name}</strong>: {contact.email}
+          </li>
+        ))}
+      </ul>
+        </CardContent>
       </Card>
     )
 }
-
