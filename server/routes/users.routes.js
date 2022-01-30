150d25cc99cea33cc180fd6c305fc5c79f4d1ebb
Asif <1421015@iub.edu.bd>
2022-01-30 15:02:42 +0600

    [server] done:insert multiple employee info

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import pool from '../db.js';
+import format  from 'pg-format'
 
 
 const router = express.Router();
@@ -18,7 +19,7 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     try {
        const {firstName,lastName,email}=req.body
-      const newUser = await pool.query(
+       const newUser = await pool.query(
         'INSERT INTO users (firstName, lastName, email) VALUES ($1,$2,$3) RETURNING *'
         , [firstName, lastName, email]);
      
@@ -28,5 +29,20 @@ router.post('/', async (req, res) => {
       res.status(500).json({error: error.message});
     }
   });
+
+router.post('/multi-insert', async (req, res) => { 
+    try {
+      
+      const { users } = req.body
+      const result = users.map((item) => {
+        return [item.firstName, item.lastName, item.email]
+      })
+      const sql = format('INSERT INTO users (firstName, lastName,email) VALUES %L', result)
+      const newUser = await pool.query(sql)
+      res.json(newUser)
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  })
 
 export default router;
