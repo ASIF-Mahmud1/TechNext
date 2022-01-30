39e88779089aa960d01aba5f34e03da5715ab2a3
Asif <1421015@iub.edu.bd>
2022-01-30 19:01:48 +0600

    [client] relevant routes added

--- client/src/MainRouter.js
+++ client/src/MainRouter.js
@@ -10,16 +10,24 @@ import UploadCSV from './employee/component/UploadCSV'
 import Employee from './employee/component/Employee'
 import SendEmail from './employee/component/SendEmail'
 import SearchEmployee from './employee/component/SearchEmployee'
+import Dashboard from "./employee/container/Dashborad";
+import AddNewEmployee from './employee/component/AddNewEmployee'
+import Form from './employee/component/Form'
+
 export default function MainRouter() {
   return (
     <Router>
       <div>
         <TopBar>
             <Routes>
-              {/* <Route path="/" element={ <UploadCSV />}/> */}
-              {/* <Route path ="/" element ={<Employee/>} />  */}
-              {/* <Route path="/" element={<SendEmail/>  }  /> */}
-              <Route path="/" element={ <SearchEmployee/> }  />
+              <Route path="/" element={  <Dashboard/>}  />
+              <Route path="/byCsv" element={ <UploadCSV />}/>
+              <Route path ="/employee" element ={<Employee/>} /> 
+              <Route path="/addEmployee" element={<AddNewEmployee/>  }  />
+              <Route path="/form" element={<Form/>  }  />
+              <Route path="/sendEmail" element={<SendEmail/>  }  />
+              <Route path="/searchEmployee" element={ <SearchEmployee/> }  />
+
             </Routes>
         </TopBar>
       </div>
